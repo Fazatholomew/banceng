8e83c8a66873de5bde0ddce7a92f00a3ea92199b
Faza Byandika Hikmatullah <thereverend.sullivan@yahoo.co.id>
2020-05-02 17:33:57 -0400

    Setting up build branch

---
+++ precache-manifest.144805d7635e4e068331da8ad861f65c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e162ce5b06e61880d3037e90916758bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f154a65392b219e10d",
+    "url": "/static/css/main.7a7d1b2c.chunk.css"
+  },
+  {
+    "revision": "5fb4bd4e8c305d1521e8",
+    "url": "/static/js/2.2f1c1f65.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.2f1c1f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f154a65392b219e10d",
+    "url": "/static/js/main.6654f838.chunk.js"
+  },
+  {
+    "revision": "c4bac71e0a56d56ca85a",
+    "url": "/static/js/runtime-main.2771b555.js"
+  },
+  {
+    "revision": "c770dcd2a610d6cdded0641b768baa5d",
+    "url": "/static/media/Poker.c770dcd2.woff"
+  },
+  {
+    "revision": "91920344759b8220b7ce7d80551ffd7d",
+    "url": "/static/media/background.91920344.png"
+  },
+  {
+    "revision": "6ca016115a894f69dea75cc80f95ad92",
+    "url": "/static/media/cardback.6ca01611.jpg"
+  },
+  {
+    "revision": "0431c28f3a7f587878b5fbe6e9945ab8",
+    "url": "/static/media/custom.0431c28f.ttf"
+  }
+]);
